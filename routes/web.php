476ee2f44c8cfec455705f5f82629b3a06b50819
lzx8444 <844470479@qq.com>
2018-09-11 10:58:56 +0000

    Finish layout and routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
